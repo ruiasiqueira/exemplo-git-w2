f9e4d55a415eceaa36cd282a29cae0033bbb5137
ruiasiqueira <ruiamorim20046@gmail.com>
2023-03-07 20:49:40 -0300

    exemplo de saídas no console

---
+++ script.js
@@ -0,0 +1,12 @@
+// Este é um comentário de linha em JavaScript
+
+/**
+ * Este é um
+ * Comentário de bloco em Javascript
+ 
+
+
+
+console.log ("Olá mundo!")
+console.error("Deu erro")
+console.warn("Atenção")
